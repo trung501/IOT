6c9067ac1de84ebbd1daf4c624c5ecf9ccded0da
trung501 <gotrungogle@gmail.com>
2022-07-29 22:40:24 +0700

    test

--- raspy.py
+++ raspy.py
@@ -14,19 +14,36 @@ radio.channel = 90
 network.begin(this_node)
 radio.printPrettyDetails()
 radio.startListening()  # put radio in RX mode
+
+packets_sent = 0
+last_sent = 0
 try:
     while True:
+        # radio.startListening() 
+        #Nhan goi tin
         network.update()
         while network.available():
             header, payload = network.read(4)            
-            print("payload length ", len(payload))
             print(
                 f"Received payload {list(payload)[0]} from {oct(header.from_node)}",
                 f"to {oct(header.to_node)} ",
             )
-            # print("payload:",payload)
+        time.sleep(0.1)
+        #radio.stopListening() 
+
+        # Gui goi tin
+        network.update()
+        now = int(time.monotonic_ns() / 1000000)
+        # If it's time to send a message, send it!
+        if now - last_sent >= interval:
+            last_sent = now
+            packets_sent += 1
+            ok = network.write(RF24NetworkHeader(other_node), packets_sent)
+            print(f"Sending {packets_sent}...", "ok." if ok else "failed.")
+        
 except KeyboardInterrupt:
     print("powering down radio and exiting.")
     radio.powerDown()
+
 
 
