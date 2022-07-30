cfbda94987b2619067c998b8d7b984397cf0020a
trung501 <gotrungogle@gmail.com>
2022-07-30 12:47:33 +0700

    test

--- raspy_OPP.py
+++ raspy_OPP.py
@@ -19,8 +19,10 @@ class IOT_Rasp:
         self.network.begin(self.this_node)
         self.radio.printPrettyDetails()
         self.radio.startListening()
+
         self.xacThuc1=52836
         self.xacThuc2=147
+
         self.interval=2000
         self.last_sent = 0
         self.packets_sent = 0
@@ -34,34 +36,40 @@ class IOT_Rasp:
         if data[0]==self.xacThuc1 and data[2]==self.xacThuc2:
             return True,data
     
+    def receive(self):
+        self.network.update()
+        while self.network.available():
+            header, payload = self.network.read(10)  
+            checkReceive,data = self.checkXacThuc(payload)
+            if checkReceive==True:
+                print(
+                f"Received value {data[3]} of device {data[1]} from {oct(header.from_node)}",
+                f"to {oct(header.to_node)} "
+                )
+            else:
+                print("Nhan payload",payload)
+    
+    def send(self):
+        self.network.update()
+        now = int(time.monotonic_ns() / 1000000)
+        # If it's time to send a message, send it!
+        if now - self.last_sent >= self.interval:
+            self.last_sent = now
+            self.packets_sent += 1
+            payload = struct.pack("HBBH", self.xacThuc1 ,0,self.xacThuc2,self.packets_sent)
+            ok = self.network.write(RF24NetworkHeader(node1), payload)
+            print(f"Sending  {self.packets_sent} to {node1}...", "ok." if ok else "failed.")
+
     def run(self):
         try:
             while True:
                 #Nhan goi tin
-                self.network.update()
-                while self.network.available():
-                    header, payload = self.network.read(10)  
-                    checkReceive,data = self.checkXacThuc(payload)
-                    if checkReceive==True:
-                        print(
-                        f"Received value {data[3]} of device {data[1]} from {oct(header.from_node)}",
-                        f"to {oct(header.to_node)} "
-                        )
-                    else:
-                        print("Nhan payload",payload)
+                self.receive()
                     
                 time.sleep(0.1)
 
                 # Gui goi tin
-                self.network.update()
-                now = int(time.monotonic_ns() / 1000000)
-                # If it's time to send a message, send it!
-                if now - self.last_sent >= self.interval:
-                    self.last_sent = now
-                    self.packets_sent += 1
-                    payload = struct.pack("HBBH", self.xacThuc1 ,0,self.xacThuc2,self.packets_sent)
-                    ok = self.network.write(RF24NetworkHeader(node1), payload)
-                    print(f"Sending  {self.packets_sent} to {node1}...", "ok." if ok else "failed.")
+                self.send()
                 
         except KeyboardInterrupt:
             print("powering down radio and exiting.")
