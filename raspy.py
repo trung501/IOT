9e4611fdfd6bf4431f932694a47268c8c77fb873
trung501 <gotrungogle@gmail.com>
2022-07-30 19:07:46 +0700

    test

--- raspy.py
+++ raspy.py
@@ -1,70 +1,96 @@
+import this
 from RF24 import RF24
 from RF24Network import RF24Network,RF24NetworkHeader
 import struct
 import time
 
-radio = RF24(22, 0,1000000)
-network = RF24Network(radio)
-this_node = 0o0
+node1 = 0o1
+node2 = 0o2
+node21=0o21
 
-other_node1 = 0o1
+class IOT_Rasp:
+    def __init__(self,CE,CSN,this_node):
+        self.CE=CE
+        self.CSN=CSN
+        self.this_node= this_node
+        self.radio = RF24(CE, CSN,1000000)
+        self.network=RF24Network(self.radio)
+        if not self.radio.begin():
+            raise RuntimeError("radio hardware not responding")       
+        self.radio.channel = 90
+        self.network.begin(self.this_node)
+        self.radio.printPrettyDetails()
+        self.radio.startListening()
 
-if not radio.begin():
-    raise RuntimeError("radio hardware not responding")
+        self.xacThuc1=52836
+        self.xacThuc2=147
 
-radio.channel = 90
-network.begin(this_node)
-radio.printPrettyDetails()
-radio.startListening()  # put radio in RX mode
+        self.interval=2000
+        self.last_sent = 0
+        self.packets_sent = 0
 
-xacThuc1=52836
-xacThuc2=147
+    def checkXacThuc(self,data):
+        if len(data) != 6:
+            return False,0
+        data= struct.unpack("HBBH",data)
+        if data[0]==self.xacThuc1 and data[2]==self.xacThuc2:
+            return True,data
+    
+    def handleDataReceiveFromNode1(self,data):
+        pass
+    def handleDataReceiveFromNode2(self,data):
+        pass
+    def handleDataReceiveFromNode21(self,data):
+        pass
 
-def checkXacThuc(data):
-    if len(data) != 6:
-        return False,0
-    data= struct.unpack("HBBH",data)
-    if data[0]==xacThuc1 and data[2]==xacThuc2:
-        return True,data
-
-
-interval = 2000  # in milliseconds
-packets_sent = 0
-last_sent = 0
-try:
-    while True:
-        # radio.startListening() 
-        #Nhan goi tin
-        network.update()
-        while network.available():
-            header, payload = network.read(10)  
-            checkReceive,data = checkXacThuc(payload)
+    def receiveFromMCU(self):
+        self.network.update()
+        while self.network.available():
+            header, payload = self.network.read(10)  
+            checkReceive,data = self.checkXacThuc(payload)
             if checkReceive==True:
                 print(
                 f"Received value {data[3]} of device {data[1]} from {oct(header.from_node)}",
-                f"to {oct(header.to_node)} ",
-            )
+                f"to {oct(header.to_node)} "
+                )
+                if header.from_node==node1:
+                    self.handleDataReceiveFromNode1(data)
+                elif  header.from_node==node2:
+                    self.handleDataReceiveFromNode2(data)
+                elif header.from_node==node21:
+                    self.handleDataReceiveFromNode21(data)
+                else :
+                    print("Khong co xu ly tu node",header.from_node)
             else:
-                print("Nhan payload",payload)
-            
-        time.sleep(0.1)
-        #radio.stopListening() 
+                print("Nhan payload",payload,"from node",oct(header.from_node))
+    
+    def sendToNode(self,node,device,value ):
+        payload = struct.pack("HBBH", self.xacThuc1 ,device,self.xacThuc2,self.packets_sent)
+        ok = self.network.write(RF24NetworkHeader(node), payload)
+        print(f"Sending  {value} to device {device} of node {oct(node)}...", "ok." if ok else "failed.")
 
-        # Gui goi tin
-        network.update()
+    def sendToMCU(self):
+        self.network.update()
         now = int(time.monotonic_ns() / 1000000)
-        # If it's time to send a message, send it!
-        if now - last_sent >= interval:
-            last_sent = now
-            packets_sent += 1
+        if now - self.last_sent >= self.interval:
+            self.last_sent = now
+            self.packets_sent += 1
+            # self.sendToNode(node1,0,self.packets_sent)  
+            self.sendToNode(node2,0,self.packets_sent)  
+            self.sendToNode(node21,0,self.packets_sent+15)           
 
-            payload = struct.pack("HBBH", xacThuc1 ,0,xacThuc2,packets_sent)
-            ok = network.write(RF24NetworkHeader(other_node1), payload)
-            print(f"Sending  {packets_sent} to {other_node1}...", "ok." if ok else "failed.")
+    def run(self):
+        try:
+            while True:
+                self.receiveFromMCU()    
+                self.sendToMCU()                
+        except KeyboardInterrupt:
+            print("powering down radio and exiting.")
+            self.radio.powerDown()
+
+
+IOT=IOT_Rasp(22, 0,0o0)
+IOT.run()
+
+
         
-except KeyboardInterrupt:
-    print("powering down radio and exiting.")
-    radio.powerDown()
-
-
-
