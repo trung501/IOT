8ffe45d28606373a21303cfcc889819fab00d06a
QuangHung <doquanghung25102001@gmail.com>
2022-08-01 23:53:55 +0700

    test

--- paho_mqtt.py
+++ paho_mqtt.py
@@ -29,17 +29,17 @@ class MQTT_client():
             print(f"Failed to send message to topic {self.topic}")
         self.client.loop_stop()
     def subscribe(self,topic='control'):
-        self.client.loop_start()
         self.topic = topic
         result = []
         def on_message(client, userdata, msg):
             #print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic on server mqtt")
             result.append(msg.payload.decode())
+            #print(result[-1])
         self.client.subscribe(self.topic)
         self.client.on_message = on_message
-        self.client.loop_stop()
+        self.client.loop_forever()
         if len(result) > 0:
-            return result.pop()
+            return result
         return 5
 
 def run():
